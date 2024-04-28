1cbfd06f48a62bc0845fbe286f2d017d8d553c16
Tamangpurnima <tamangpurnima596@gmail.com>
2024-04-28 13:41:42 +0545

    changes: in Scripts

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Designer", "Freelancer", "Learner"],
+        strings: ["Developer", "Designer", "Learner"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer", "Designer", "Freelancer","Learner"],
+        strings: ["Web Developer", "Designer", "Learner"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
